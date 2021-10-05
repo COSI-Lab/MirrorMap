6f1654aebfa1c5900eaa3dfb30ca8f11b910adc3
mahonec <mahonec@clarkson.edu>
2021-10-04 22:30:14 -0400

    server

--- server.go
+++ server.go
@@ -10,11 +10,22 @@ import (
 	"os"
 	"os/signal"
 	"strings"
-	"time"
+	"sync"
+	"syscall"
 
+	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 	"github.com/oschwald/geoip2-golang"
+	"github.com/thanhpk/randstr"
 )
+
+// Globals
+var clients map[string]chan []byte
+var clients_lock sync.RWMutex
+
+var upgrader = websocket.Upgrader{} // use default options
+var interrupt2 chan os.Signal
+var done2 chan interface{}
 
 func parser(line string) string {
 	foundIp := strings.SplitN(line, "-", 2)[0]
@@ -26,20 +37,17 @@ type longLat struct {
 	Lat  float64
 }
 
-func fileIn(ch chan []byte) {
-
-	scanner := bufio.NewScanner(os.Stdin)
+func fileIn(clients map[string]chan []byte) {
 	db, err := geoip2.Open("logs/GeoLite2-City.mmdb")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// buf := bytes.NewBuffer(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
 		ip := parser(line)
-		// fmt.Println(ip)
 		if ip == "" {
 			continue
 		}
@@ -51,28 +59,28 @@ func fileIn(ch chan []byte) {
 		}
 		long := results.Location.Longitude
 		lat := results.Location.Latitude
-		// fmt.Println(long, lat)
-		ch <- []byte(fmt.Sprintf("%f:%f", long, lat))
+		fmt.Println(long, lat)
+
+		clients_lock.RLock()
+		for _, ch := range clients {
+			ch <- []byte(fmt.Sprintf("%f:%f", long, lat))
+		}
+		clients_lock.RUnlock()
 	}
 }
 
-var upgrader = websocket.Upgrader{} // use default options
-var interrupt2 chan os.Signal
-var done2 chan interface{}
-
 func socketHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
 
-	ch := make(chan []byte)
+	if id == "" {
+		w.WriteHeader(404)
+		return
+	}
 
-	go func() {
-		// defer wg.Done()
-		fileIn(ch)
+	// get the channel
+	ch := clients[id]
 
-	}()
-
-	interrupt2 = make(chan os.Signal) // Channel to listen for interrupt signal to terminate gracefully
-	done2 = make(chan interface{})
-	signal.Notify(interrupt2, os.Interrupt)
 	// Upgrade our raw HTTP connection to a websocket based one
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -83,51 +91,30 @@ func socketHandler(w http.ResponseWriter, r *http.Request) {
 
 	for {
 		val := <-ch
-		select {
-		case <-time.After(time.Duration(1) * time.Millisecond * 1000):
-			conn.WriteMessage(1, val)
-
-		case <-interrupt2:
-			// We received a SIGINT (Ctrl + C). Terminate gracefully...
-			log.Println("Received SIGINT interrupt signal. Closing all pending connections")
-
-			// Close our websocket connection
-			err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("Error during closing websocket:", err)
-				return
-			}
-
-			select {
-			case <-done2:
-				log.Println("Receiver Channel Closed! Exiting....")
-			case <-time.After(time.Duration(1) * time.Second):
-				log.Println("Timeout in closing receiving channel. Exiting....")
-			}
+		err = conn.WriteMessage(1, val)
+		if err != nil {
+			delete(clients, id)
 			return
 		}
 	}
+}
 
-	// // The event loop
-	// for {
-	// 	messageType, message, err := conn.ReadMessage()
-	// 	if err != nil {
-	// 		log.Println("Error during message reading:", err)
-	// 		break
-	// 	}
-	// 	m := fmt.Sprintf("%s", message)
-	// 	nums := strings.Split(m, ":")
-	// 	num1, num2 := nums[0], nums[1]
-	// 	log.Printf("Long: %s : Lat: %s, received on 2", num1, num2)
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	id := randstr.Hex(16)
 
-	// 	err = conn.WriteMessage(messageType, message)
-	// 	if err != nil {
-	// 		log.Println("Error during message writing:", err)
-	// 		break
-	// 	} else {
-	// 		conn.WriteMessage(messageType, []byte("Test"))
-	// 	}
-	// }
+	clients_lock.Lock()
+	clients[id] = make(chan []byte)
+	clients_lock.Unlock()
+
+	// Send id to client
+	w.WriteHeader(200)
+	w.Write([]byte(id))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	// Send id to client
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprint(len(clients))))
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -135,8 +122,32 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Create a type safe Map for strings to channels
+	clients = make(map[string]chan []byte)
 
-	http.HandleFunc("/socket", socketHandler)
-	http.HandleFunc("/", home)
-	log.Fatal(http.ListenAndServe("128.153.165.236:8080", nil))
+	interrupt := make(chan os.Signal) // Channel to listen for interrupt signal to terminate gracefully
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-interrupt
+		fmt.Println("\r- Ctrl+C pressed in Terminal")
+		os.Exit(1)
+	}()
+
+	// Read from standard in and pass cordinates to each client
+	go fileIn(clients)
+
+	// gorilla/mux router
+	r := mux.NewRouter()
+	r.HandleFunc("/health", healthHandler)
+	r.HandleFunc("/register", registerHandler)
+	r.HandleFunc("/socket/{id}", socketHandler)
+	r.HandleFunc("/", home)
+
+	// Serve on 8080
+	s := &http.Server{
+		Addr:    ":8000",
+		Handler: r,
+	}
+	log.Fatalf("%s", s.ListenAndServe())
 }
